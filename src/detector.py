861d8daebd21d1c7cfa193f4801950be9a57e9c8
zhamdaan123 <zhamdaan123@gmail.com>
2024-10-26 01:25:41 +0530

    .

--- src/detector.py
+++ src/detector.py
@@ -1,12 +1,8 @@
-from tflite_runtime.interpreter import Interpreter
+import tensorflow as tf
 
 from src.commons import convert_to_ascii
 
-#model = tf.keras.models.load_model('model.h5')
-interpreter = Interpreter(model_path='model.tflite')
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+model = tf.keras.models.load_model('model.h5')
 
 
 class XSSDetector:
@@ -16,9 +12,6 @@ class XSSDetector:
         """Predict a data"""
         ascii_input = convert_to_ascii(data)
         ascii_input = ascii_input.reshape(1, 20, 20, 1)
-        #prediction = model.predict(ascii_input)[0][0]
-        interpreter.set_tensor(input_details[0]['index'], ascii_input)
-        interpreter.invoke()
-        prediction = interpreter.get_tensor(output_details[0]['index'])[0]
+        prediction = model.predict(ascii_input)[0][0]
         is_malicious = True if prediction > score else False
         return is_malicious, float(prediction)
