f3093f2e0d8b4dbfb4a5f963adb4b68764b9c957
zhamdaan123 <zhamdaan123@gmail.com>
2024-10-26 01:16:02 +0530

    .

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.12
+FROM python:3.9
 WORKDIR /app
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 EXPOSE 8000
 CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
--- src/detector.py
+++ src/detector.py
@@ -1,8 +1,12 @@
-import tensorflow as tf
+from tflite_runtime.interpreter import Interpreter
 
 from src.commons import convert_to_ascii
 
-model = tf.keras.models.load_model('model.h5')
+#model = tf.keras.models.load_model('model.h5')
+interpreter = Interpreter(model_path='model.tflite')
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 
 class XSSDetector:
@@ -12,6 +16,9 @@ class XSSDetector:
         """Predict a data"""
         ascii_input = convert_to_ascii(data)
         ascii_input = ascii_input.reshape(1, 20, 20, 1)
-        prediction = model.predict(ascii_input)[0][0]
+        #prediction = model.predict(ascii_input)[0][0]
+        interpreter.set_tensor(input_details[0]['index'], ascii_input)
+        interpreter.invoke()
+        prediction = interpreter.get_tensor(output_details[0]['index'])[0]
         is_malicious = True if prediction > score else False
         return is_malicious, float(prediction)
